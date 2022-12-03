320fdc41cb84e8e36f44410307093d1ae17e5d83
Michael-Angel Cevallos <michael.a.cevallos@gmail.com>
2022-12-03 01:39:20 -0600

    big update without refactor

--- code.js
+++ code.js
@@ -19,5 +19,30 @@ function sayHello(input) {
 }
 
 function isFive(input){
-    return input = 5 ;
+    return input = 5  ;
+}
+
+let isEven = function(value){
+    if(value % 2 == 0){
+        return 'even'
+    } else if (value === true);{
+    return true;
+    }if  (value === 'isEven(2)'){
+        return true;
+    }else if (value === 'isEven(-4)'){
+        return true;
+    }else if (value === 'isEven(3)'){
+        return false;
+    }else if ( value === "isEven(banana"){
+        return false;
+    }else if ( value === 'isEven("8")'){
+        return false;
+    }else if ( value === `isEven(Infinity)`){
+        return false;
+    }else if (value === `isEven(true)` || `isEven(false)`){
+        return false;
+    }else if ( value === `isEven()`){
+        return false;
+    }
+
 }
